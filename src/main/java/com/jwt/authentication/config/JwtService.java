5ab9d7046d9f0d94c050d892bcb5d3579b48b04b
leowader <waderhe@gmail.com>
2024-05-08 11:54:02 -0500

    configuracion de jwt

---
+++ src/main/java/com/jwt/authentication/config/JwtService.java
@@ -0,0 +1,53 @@
+package com.jwt.authentication.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+    private static final String SECRET_KEY="d57fd09cb3dda3a3ed629b6c7a84d866b2b1cc1d106a09528e507bc1cd037f86";
+
+    public String getUsername(String token) {
+
+        return getClaim(token, Claims::getSubject);
+    }
+
+
+    public <T> T getClaim(String token, Function<Claims,T> claimsResolve){
+        final Claims claims=getAllClaims(token);
+        return claimsResolve.apply(claims);
+    }
+
+    private Claims getAllClaims(String token) {
+
+        return Jwts.parserBuilder().setSigningKey(getSigningKey())
+                .build().parseClaimsJws(token).getBody();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBites = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBites);
+    }
+    public boolean validateToken(String token, UserDetails userDetails){
+
+        final String username = getUsername(token);
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return getExpiration(token).before(new Date());
+    }
+
+    private Date getExpiration(String token) {
+        return  getClaim(token,Claims::getExpiration);
+    }
+}
---
+++ src/main/java/com/jwt/authentication/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.jwt.authentication.repository;
+
+import com.jwt.authentication.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    Optional<User> findUserByUsername(String username);
+}
